a9472b4dce7bbed2ae11fe9966eb303aefdc8de9
nboano <nboano@outlook.it>
2023-01-29 12:29:55 +0100

    Ottimizzazioni

--- src/Functions.cpp
+++ src/Functions.cpp
@@ -10,18 +10,18 @@
 /// @param linewidth Spessore della linea della funzione in pixel.
 /// @param fillStyle Colore della linea (CSS).
 #define DRAW_FN_POINT(f, W, H, image, phase, delta, linewidth, fillStyle) \
-[](bool dw) {\
+[](bool dw, bool clear) {\
     static double x = phase;\
     double y = f(x);\
     double ex = (W / (image)) * x;\
     double ey = (H/2 +  y* H/2);\
     x += delta;\
     if(ex >= W) {\
-        PageCanvas::Clear();\
         x = phase;\
         return;\
     }\
-    if(dw) PageCanvas::DrawPoint(ex,ey,linewidth,fillStyle, strlen(fillStyle));\
+    if(clear) PageCanvas::ClearXAt(ex, linewidth);\
+    if(dw) PageCanvas::DrawPoint(ex,ey,linewidth,fillStyle);\
 }\
 
 /// @brief Risoluzione orizzontale del render.
@@ -34,7 +34,7 @@ int H = 720;
 double T = M_PI * 4;
 
 /// @brief Precisione del render (delta tra i punti).
-double Precision = .005;
+double Precision = .01;
 
 exported void setT(double v) {
     PageCanvas::Clear();
@@ -109,15 +109,15 @@ namespace Functions
     /// @brief Array di funzioni visualizzate nel render.
     struct {
         bool Display;
-        void(*DrawPt)(bool);
+        void(*DrawPt)(bool, bool);
     } All[] = {
-            true, DRAW_FN_POINT(Functions::SineWave, (W-3), (H-3), T, 0, Precision, 3, "black"),
-            true, DRAW_FN_POINT(Functions::SquareWave, (W-3), (H-3), T, 0, Precision, 2, "red"),
-            true, DRAW_FN_POINT(Functions::SquareHarmonics[3], (W-3), (H-3), T, 0, Precision, 3, "green"),
-            true, DRAW_FN_POINT(Functions::SquareHarmonics[5], (W-3), (H-3), T, 0, Precision, 3, "violet"),
-            true, DRAW_FN_POINT(Functions::SquareHarmonics[7], (W-3), (H-3), T, 0, Precision, 3, "yellowgreen"),
-            true, DRAW_FN_POINT(Functions::SquareFourier, (W-3), (H-3), T, 0, Precision, 5, "blue"),
-            false, DRAW_FN_POINT(Functions::SawToothFourier, (W-3), (H-3), T, 0, Precision, 5, "orange"),
+        true, DRAW_FN_POINT(Functions::SineWave, (W-6), (H-6), T, 0, Precision, 6, "black"),
+        true, DRAW_FN_POINT(Functions::SquareWave, (W-6), (H-6), T, 0, Precision, 4, "red"),
+        true, DRAW_FN_POINT(Functions::SquareHarmonics[3], (W-6), (H-6), T, 0, Precision, 6, "green"),
+        true, DRAW_FN_POINT(Functions::SquareHarmonics[5], (W-6), (H-6), T, 0, Precision, 6, "violet"),
+        true, DRAW_FN_POINT(Functions::SquareHarmonics[7], (W-6), (H-6), T, 0, Precision, 6, "yellowgreen"),
+        true, DRAW_FN_POINT(Functions::SquareFourier, (W-6), (H-6), T, 0, Precision, 10, "blue"),
+        false, DRAW_FN_POINT(Functions::SawToothFourier, (W-6), (H-6), T, 0, Precision, 10, "orange"),
     };
 
     exported void DisplayFunction(int index, bool v) {
@@ -139,9 +139,17 @@ namespace Functions
     }
 
     void Render(void*) {
+        double start = microtime();
+
         for (int i = 0; i < 100; i++)
          for(int j = 0; j < sizeof(All) / sizeof(All[0]); j++) 
-                All[j].DrawPt(All[j].Display);
+                All[j].DrawPt(All[j].Display, j == 0);
+
+        double frameTime = microtime() - start;
+
+        $("#frameTime").innerText = (string)round(frameTime);
+        $("#frameRate").innerText = (string)round(1000 / frameTime);
+
         setTimeout(Render, Interval);
     }
 
--- src/PageCanvas.cpp
+++ src/PageCanvas.cpp
@@ -9,8 +9,12 @@ namespace PageCanvas
         canvas.Init();
     }
 
-    void DrawPoint(double x, double y, int size, const char* color, int len) {
+    void DrawPoint(double x, double y, int size, const char* color) {
         canvas.DrawRectangle(x, y, size, size, color);
+    }
+
+    void ClearXAt(double x, int size) {
+        canvas.DrawRectangle(x, 0, size, canvas.Height, "#FFF");
     }
 
     void Clear() {
